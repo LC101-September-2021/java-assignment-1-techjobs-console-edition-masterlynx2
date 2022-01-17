8182b59626f813f811275f4949fc637aa8d919da
Masterlynx2 <Tonywatkins325@gmail.com>
2022-01-17 18:14:11 -0500

    please just work version 101

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -127,7 +127,7 @@ public class TechJobs {
         ArrayList<HashMap<String, String>> jobs = someJobs;
         for (HashMap<String, String> job : jobs) {
 
-            System.out.println("\n*****");
+            System.out.println("*****");
 
 
             for (Map.Entry<String, String> printJobs : job.entrySet()) {
